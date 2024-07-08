339fc1196db165f9bd38834857f2c3f06cc5c6d7
Daniel P <147325072+dani238qr@users.noreply.github.com>
2024-07-08 21:17:20 +0300

    Update centered_time.sh

--- i3blocks/centered_time.sh
+++ i3blocks/centered_time.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# centered_time.sh
+
+# Get the current date and time
+datetime=$(date '+%Y-%m-%d %I:%M %p')
+
+# Calculate padding to center the text
+padding="                    "  # 20 spaces
+p1="    				" # 19 spaces
+centered_datetime="$padding$datetime$padding$padding$p1"
+
+# Output the centered date and time
+echo "$centered_datetime"
+
 
