0c9776c6e23c7acff984e143b95b942270c2e3fb
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-08-11 20:36:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -194,14 +194,18 @@ brave_debian() {
 
     sudo apt install curl
 
+    cd
+
     sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
     echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
     sudo apt update
 
-    sudo apt install brave-browser
+    sudo apt install brave-browser -y
     echo "Brave browser installed successfully."
+
+    cd -
 }
 
 
